53a9683435b3d6557cb972726a829312e2eca60f
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2023-01-16 18:58:34 -0500

    Update layers.py

--- basic_neurals/layers.py
+++ basic_neurals/layers.py
@@ -143,38 +143,36 @@ class FullyConnectedLayer(Layer):
     def backward(self, gradIn):
         pass
 
-#Let’s test each layer using the following input data (this can be considered two observations, with four features per observation):
-
-#Imports your layers module.
-
 X = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
 
 
 #Input Layer
-# inputLayer = inputLayer(X)
+inputLayer = inputLayer(X)
 # print(inputLayer.forward(X))
 
 #Linear Layer
-# linearLayer = LinearLayer(4, 3)
+linearLayer = LinearLayer(4, 3)
 # print(linearLayer.forward(X))
 
 #ReLu Layer
-# reluLayer = ReLULayer()
+reluLayer = ReLULayer()
 # print(reluLayer.forward(X))
 
 #Logistic Sigmoid Layer
-# logisticSigmoidLayer = LogisticSigmoidLayer()
+logisticSigmoidLayer = LogisticSigmoidLayer()
 # print(logisticSigmoidLayer.forward(X))
 
 #Softmax Layer
-# softmaxLayer = SoftmaxLayer()
+softmaxLayer = SoftmaxLayer()
 # print(softmaxLayer.forward(X))
 
 #Tanh Layer
-# tanhLayer = TanhLayer()
+tanhLayer = TanhLayer()
 # print(tanhLayer.forward(X))
 
 #Fully Connected Layer
 fullyConnectedLayer = FullyConnectedLayer(4, 2)
 print(fullyConnectedLayer.forward(X))
+
+
